b8562cae76a770ece6dfc2b23bee3ec522578e88
eal6gf <elum1234@gmail.com>
2018-01-02 17:02:00 -0500

    starting ratings forms

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,13 @@
 class RatingsController < ApplicationController
+
+  def new
+    @rating = Rating.new
+  end
+
+
+  private
+
+  def rating_params
+    params.require(:rating).permit(:discipline, :learning_style, :grit, :gpa, :subject)
+  end
 end
