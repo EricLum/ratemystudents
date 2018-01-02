e9b0b43bc8cdedd61025d5aab9508a86baf27cae
eal6gf <elum1234@gmail.com>
2018-01-02 17:18:45 -0500

    validations and forms

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -4,6 +4,45 @@ class RatingsController < ApplicationController
     @rating = Rating.new
   end
 
+  def create
+    @rating = Rating.new(rating_params)
+
+    if @rating.save
+      redirect_to rating_path(@rating)
+    else
+      flash[:error] = @rating.errors.full_messages
+      redirect_to new_rating_path
+    end
+  end
+
+  def show
+    @rating = Rating.find(params[:id])
+  end
+
+  def index
+    @ratings = Rating.all
+  end
+
+  def edit
+    @rating = Rating.find(params[:id])
+  end
+
+  def update
+    @rating = Rating.find(params[:id])
+    if @rating.update(rating_params)
+      redirect_to rating_path(@rating)
+    else
+      flash[:error] = @rating.errors.full_messages
+      redirect_to edit_rating_path(@rating)
+    end
+  end
+
+  def destroy
+    @rating = Rating.find(params[:id])
+    @rating.destroy
+    redirect_to ratings_path
+  end
+
 
   private
 
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,5 @@
 class Rating < ApplicationRecord
   has_many :student_ratings
   has_many :students, through: :student_ratings
+  validates :gpa, numericality: {greater_than_or_equal_to: 0}
 end
