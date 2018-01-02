39934ac59ff6fa356e52269ddafcb90f4023d1c0
eal6gf <elum1234@gmail.com>
2018-01-02 16:50:56 -0500

    barely working auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,24 @@
 class SessionsController < ApplicationController
   skip_before_action :authorized, only: [:new, :create]
-
   def new
   end
 
   def create
-
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
+    @teacher = Teacher.find_by(username: params[:username])
+    if @teacher && @teacher.authenticate(params[:password])
+      # YOU ARE WHO YOU SAY YOU ARE
+      session[:teacher_id] = @teacher.id
+      redirect_to teacher_path(@teacher)
     else
-
       flash[:error] = ["Username and password do not match"]
-      redirect_to login_path
+      redirect_to signin_path
     end
   end
 
-
   def destroy
-    session[:user_id] = nil
-    redirect_to login_path
+    # get logged out
+    session[:teacher_id] = nil
+    # redirect to login
+    redirect_to signin_path
   end
 end
