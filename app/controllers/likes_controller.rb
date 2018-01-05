db4fded0e3058b295375fd85f85513e702a158ed
mattfaircloth <mattcfaircloth@gmail.com>
2018-01-05 13:47:06 -0500

    fixed  like create

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,14 +4,22 @@ class LikesController < ApplicationController
   end
 
   def create
-    @like = Like.find_or_create_by(like_params)
-    if @like.valid?
-      @like.save
-    else
-      #do nothing
-    end
-    redirect_to comment_path(@like.comment)
-  end
+   @current_like = Like.find_by(comment_id: like_params[:comment_id], teacher_id: like_params[:teacher_id])
+
+   if @current_like
+     @comment = @current_like.comment
+     @current_like.destroy
+   else
+     @like = Like.new(like_params)
+     if @like.valid?
+       @like.save
+       @comment = @like.comment
+     else
+       #do nothing
+     end
+   end
+   redirect_to comment_path(@comment)
+ end
 
   private
 
