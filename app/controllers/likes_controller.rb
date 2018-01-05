8c4b444bf53326870ff904e96ec20e0675263c13
github username <github email address>
2018-01-05 09:38:02 -0500

    like/unlike feature

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,14 +4,23 @@ class LikesController < ApplicationController
   end
 
   def create
-    @like = Like.new(like_params)
-    if @like.valid?
-      @like.save
+    @current_like = Like.find_by(comment_id: like_params[:comment_id], teacher_id: like_params[:teacher_id])
+
+    if @current_like
+      @comment = @current_like.comment
+      @current_like.destroy
     else
-      #do nothing
+      @like = Like.new(like_params)
+      if @like.valid?
+        @like.save
+        @comment = @like.comment
+      else
+        #do nothing
+      end
     end
-    redirect_to comment_path(@like.comment)
+    redirect_to comment_path(@comment)
   end
+
 
   private
 
